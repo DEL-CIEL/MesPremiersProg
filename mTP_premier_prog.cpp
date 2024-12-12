af51dd4714cbd88c405c5fb089828efa28f8aace
Minimax Femto <charlemax10@gmail.com>
2024-12-12 10:55:17 +0100

    Ajoutez des fichiers projet.

---
+++ mTP_premier_prog.cpp
@@ -0,0 +1,21 @@
+// Nom du Projet : (TP_premier_prog
+// Nom de L'auteur : Delos
+// Date de création : 09/09/2024
+// Révision ou version : 
+// Version du compilateur : 
+// Résumé : premier programme en c++
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+	/*printf("Premier programme\n");
+	printf("\nSection %s promotion %d\n", "CIEL", 2026);
+	printf("\nLe point de fusion du mercure est de %f\xB0 C soit environ %g\xB0 C\n", -38.842, -38.842);
+	_getch();*/
+	printf("Total : %d \n ", printf("Premier programme\n"));
+	return 0;
+}
